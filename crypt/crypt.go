9581da6fab09ca21a4698d5c3e08ca08526cbbb2
ReeseHatfield <hatfield.69@wright.edu>
2024-06-23 13:56:55 -0400

    crypt now uses bytes isntead of strings

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -4,52 +4,44 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/hex"
 	"errors"
 	"io"
 )
 
-type PlainText string
-type CypherText string
+type PlainText []byte
+type CypherText []byte
 
 func Encrypt(text PlainText, key []byte) (CypherText, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	// convert text to raw bytes, maybe just pass this as a byte array
-	plainText := []byte(text)
-	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	cipherText := make([]byte, aes.BlockSize+len(text))
 	iv := cipherText[:aes.BlockSize]
 
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return "", err
+		return nil, err
 	}
 
 	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], text)
 
-	return CypherText(hex.EncodeToString(cipherText)), nil
+	return CypherText(cipherText), nil
 }
 
 func Decrypt(text CypherText, key []byte) (PlainText, error) {
-	cipherText, err := hex.DecodeString(string(text))
-	if err != nil {
-		return "", err
+	if len(text) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
 	}
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	if len(cipherText) < aes.BlockSize {
-		return "", errors.New("ciphertext too short")
-	}
-
-	iv := cipherText[:aes.BlockSize]
-	cipherText = cipherText[aes.BlockSize:]
+	iv := text[:aes.BlockSize]
+	cipherText := text[aes.BlockSize:]
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 	stream.XORKeyStream(cipherText, cipherText)
