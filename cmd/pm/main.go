8889f7e0ded6013e1a0ea20c9bfd36b64829bef7
ReeseHatfield <hatfield.69@wright.edu>
2024-06-23 11:53:02 -0400

    add pm.dat detection:

--- cmd/pm/main.go
+++ cmd/pm/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/user"
 	"pm/files"
 	"pm/pm"
+	"pm/utils"
 )
 
 func main() {
@@ -14,10 +18,37 @@ func main() {
 	}
 	fmt.Printf("SHA-256 hash of serial number: %x\n", key)
 
-	pmDotDat := "~/.pm/pm.dat"
+	scanner := bufio.NewScanner(os.Stdin)
+
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pmDotDat := usr.HomeDir + "/.pm/pm.dat"
 	if !files.FileExists(pmDotDat) {
 		fmt.Println(pmDotDat, "was not found")
 		// walk thru creation process with encryption
+		fmt.Println("Would you like to set up pm?")
+
+		var line string
+		for scanner.Scan() {
+
+			line = scanner.Text()
+
+			if utils.IsValidYesNoString(line) {
+				break
+			}
+		}
+
+		if rune(line[0]) == 'y' {
+			// create and encrypt file
+			fmt.Println("generating file")
+		} else {
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("pm.dat WAS found")
 	}
 
 }
